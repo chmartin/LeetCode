acd07b46e04cf52ad129162beec800788113ce67
Christopher Martin <christopher.blake.martin@cern.ch>
2017-07-26 11:31:13 +0200

    helloWorld setup

---
+++ helloWorld/helloWorld.cpp
@@ -0,0 +1,18 @@
+//Let us begin!
+/*
+
+small hello world to test local lib
+
+*/
+
+//includes
+#include<iostream>
+
+
+int main()
+{
+	//printout
+	std::cout << "Hello World" << std::endl;
+
+	return 0;
+}
